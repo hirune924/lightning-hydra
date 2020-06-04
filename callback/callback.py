338cab29617e5a3be9015627963ed04ea7e2752c
hirune924 <nori.o920924@outlook.jp>
2020-06-04 18:31:30 +0900

    add config

--- callback/callback.py
+++ callback/callback.py
@@ -8,7 +8,7 @@ class MyCallback(pl.Callback):
 
     def on_train_start(self, trainer, pl_module):
         if self.cfg.training.scheduler.class_name == 'scheduler.scheduler.CyclicLR':
-            step_size_up = int(len(trainer.train_dataloader)/2)
+            step_size_up = int(len(trainer.train_dataloader)*0.3)
             step_size_down = len(trainer.train_dataloader) - step_size_up
             trainer.lr_schedulers[0]['scheduler'] = torch.optim.lr_scheduler.CyclicLR(
                 trainer.optimizers[0],
