8d5a3ca4ed0b134d9547f7851c99637779182cdb
hirune924 <nori.o920924@outlook.jp>
2020-06-08 22:21:12 +0900

    bug fix

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -28,7 +28,7 @@ def get_datasets(cfg: DictConfig) -> dict:
 
     kf = load_obj(cfg.dataset.split.class_name)(**cfg.dataset.split.params)
 
-    for　fold, (train_index, val_index) in enumerate(kf.split(df.values, df["isup_grade"].astype(str) + df["data_provider"],)):
+    for fold, (train_index, val_index) in enumerate(kf.split(df.values, df["isup_grade"].astype(str) + df["data_provider"],)):
         df.loc[val_index, "fold"] = int(fold)
     df["fold"] = df["fold"].astype(int)
 
