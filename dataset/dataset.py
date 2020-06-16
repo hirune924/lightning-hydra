1302c3655122c82bc6e2e00753e8455d3da44286
hirune924 <nori.o920924@outlook.jp>
2020-06-16 10:37:41 +0900

    add drop option

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -36,6 +36,9 @@ def get_datasets(cfg: DictConfig) -> dict:
     train_df = df[df["fold"] != cfg.dataset.fold]
     valid_df = df[df["fold"] == cfg.dataset.fold]
 
+    if cfg.dataset.drop is not None:
+        train_df = train_df[train_df["data_provider"] != cfg.dataset.drop]
+
     train_augs_conf = OmegaConf.to_container(cfg.dataset.augmentation.train, resolve=True)
     train_augs_list = [load_obj(i["class_name"])(**i["params"]) for i in train_augs_conf]
     train_augs = A.Compose(train_augs_list)
