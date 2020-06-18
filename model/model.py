fb5633bc8aa70d2a83b5baa6ffa7a1744400c7f4
hirune924 <nori.o920924@outlook.jp>
2020-06-18 16:17:24 +0900

    add custom head

--- model/model.py
+++ model/model.py
@@ -81,16 +81,24 @@ def se_net(
         model.avg_pool = GeM()
     return model
 
-def timm_custom(model_name='gluon_seresnext50_32x4d', num_classes=1, pretrained=None, pool_size=1):
+def timm_custom(model_name='gluon_seresnext50_32x4d', num_classes=1, pretrained=None, pool_size=1, pool_type='avg', head_type='linear'):
     if pretrained is not None :
         model = timm.create_model(model_name=model_name, num_classes=num_classes, pretrained=False)
         ckpt_pth = glob.glob(utils.to_absolute_path(pretrained))
         model = load_pytorch_model(ckpt_pth[0], model)
     else:
-        model = timm.create_model(model_name=model_name, num_classes=num_classes, pretrained=True)
+        model = timm.create_model(model_name=model_name, num_classes=num_classes, pretrained=False)
 
-
-    model.global_pool = nn.AdaptiveAvgPool2d(pool_size)
-    in_features = model.fc.in_features
-    model.fc = nn.Linear(in_features*pool_size*pool_size, num_classes)
+    if pool_type == 'avg':
+        model.global_pool = nn.AdaptiveAvgPool2d(pool_size)
+    elif pool_type == 'avgdrop':
+        model.global_pool = nn.Sequential(nn.AdaptiveAvgPool2d(pool_size),
+                                          nn.Dropout2d(p=0.3, inplace=False))
+    if head_type=='linear':
+        in_features = model.fc.in_features
+        model.fc = nn.Linear(in_features*pool_size*pool_size, num_classes)
+    elif head_type=='custom':
+        in_features = model.fc.in_features
+        model.fc = nn.Sequential(nn.Linear(in_features*pool_size*pool_size, 512), nn.RReLU(lower=0.125, upper=0.3333333333333333, inplace=False),
+                                 nn.BatchNorm1d(512),nn.Linear(512,num_classes))
     return model
