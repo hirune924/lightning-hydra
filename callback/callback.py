dd0f8046f9b9a735293b2b4d6ea94d703cb8c12c
hirune924 <nori.o920924@outlook.jp>
2020-07-14 10:55:48 +0900

    tmp for batch=1

--- callback/callback.py
+++ callback/callback.py
@@ -9,8 +9,9 @@ class MyCallback(pl.Callback):
 
     def on_train_start(self, trainer, pl_module):
         if self.cfg.training.scheduler.class_name == "scheduler.scheduler.CyclicLR":
-            step_size_up = int(len(trainer.train_dataloader) * 0.3)
-            step_size_down = len(trainer.train_dataloader) - step_size_up
+            epoch_len = int(len(trainer.train_dataloader) / self.cfg.trainer.accumulate_grad_batches)
+            step_size_up = int(epoch_len * 0.3)
+            step_size_down = epoch_len - step_size_up
             trainer.lr_schedulers[0]["scheduler"] = torch.optim.lr_scheduler.CyclicLR(
                 trainer.optimizers[0], step_size_up=step_size_up, step_size_down=step_size_down, **self.cfg.training.scheduler.params
             )
